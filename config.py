45e56cd7c73ae0023f5aa51156b31354e8dd6147
ningzy <15347860+ningzy@users.noreply.github.com>
2018-09-05 19:38:03 -0700

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-know'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://rippleidea:Ripple3616@alexflask.cxhafjw1sser.us-east-1.rds.amazonaws.com:3306/alexdb'
+    SQLALCHEMY_DATABASE_URI = ''
     # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
     #     'sqlite:///' + os.path.join(basedir, 'alex.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -11,7 +11,7 @@ class Config(object):
     UPLOAD_FOLDER = os.path.join(basedir, 'test_images')
     ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
-    S3_BUCKET = 'alex-upload-flask'
-    S3_KEY = 'AKIAJGRGCUNBPS4W6XZQ'
-    S3_SECRET = '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'
+    S3_BUCKET = ''
+    S3_KEY = ''
+    S3_SECRET = ''
     S3_LOCATION = 'https://s3.amazonaws.com/{}'.format(S3_BUCKET)
