e5a04596d46d2939904723d9828c6e66c62a1e2b
Zheng Ning <airportico@gmail.com>
2018-09-05 18:56:13 -0700

    tools for s3 added

---
+++ app/tools.py
@@ -0,0 +1,26 @@
+import boto3
+from app import app
+
+s3 = boto3.client(
+    "s3",
+    aws_key = app.config['S3_KEY']
+    aws_secret = app.config['S3_SECRET']
+)
+
+def upload_to_s3(file, filename, bucket_name, acl='public-read'):
+
+    try:
+        s3.upload_fileobj(
+            file,
+            bucket_name,
+            filename,
+            ExtraArgs={
+                'ACL': acl,
+                'ContentType': file.content_type
+            }
+        )
+    except Exception as e:
+        print('Upload error: ', e)
+        return e
+    
+    return "{}{}".format(app.config['S3_LOCATION'], filename)
